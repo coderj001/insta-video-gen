3b0828eea4d214f4f06033191bdd8cf85598d97f
Raju Ghorai <amirajubolchi001@gmail.com>
2023-08-29 16:54:25 +0530

     modified: settings.py

--- src/settings.py
+++ src/settings.py
@@ -1,24 +1,55 @@
 """ Settings """
 import os
 from datetime import datetime, timedelta
+from typing import Optional
 
 from dateutil.relativedelta import relativedelta
+from pydantic import validator
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
+    """
+    Configuration and settings for the application.
+
+    Attributes:
+    - base_dir: Directory where the application is running.
+    - temp_dir: Directory for storing temporary files, such as downloaded videos.
+    - your_insta_username: Instagram username for authentication.
+    - your_insta_password: Instagram password for authentication.
+    - instagram_usernames: List of Instagram usernames whose videos are to be downloaded.
+    - start_date: Starting date for downloading Instagram videos.
+    - end_date: Ending date for downloading Instagram videos.
+    - background_music: Flag indicating whether to add background music to the compiled video.
+    - background_music_path: Path to background_music_file.
+    - outro_video_path: Path to outro video.
+    - target_resolution: Target resolution for the compiled video.
+    - your_youtube_username: YouTube username for authentication.
+    - your_youtube_password: YouTube password for authentication.
+    - limit_hashtags: Maximum number of hashtags to be used while uploading to YouTube.
+    - imp_hashtags: List of important hashtags to be included while uploading to YouTube.
+
+    Usage:
+    To access any setting, simply use: settings.<attribute_name>.
+    For example, to get the base directory, use settings.base_dir.
+    """  # noqa: E501
+
     base_dir: str = os.getcwd()
     temp_dir: str = "downloaded_videos"
+    instagram_url: str = "https://www.instagram.com"
     #####################
     # Instagram related #
     #####################
-    your_insta_username: str = ""
-    your_insta_password: str = ""
-    instagram_usernames: list[str] = [
-        "fuckjerry",
-        "ladbible",
-    ]  # added usernames in list
-    duration_str: str = os.getenv("DURATION", "4days")
+    your_insta_username: Optional[str] = None
+    your_insta_password: Optional[str] = None
+    instagram_usernames: list[str]  # added usernames in list
+
+    @validator("instagram_usernames", each_item=True)
+    def check_instagram_usernames(cls, username):
+        if not username:
+            raise ValueError("Instagram usernames must not be empty.")
+        return username
+    duration_str: str = "2days"
     # Extract the numerical part from the string
     duration: int = int(duration_str[:-4])
 
@@ -54,20 +85,57 @@ class Settings(BaseSettings):
     # Video Compilation Related #
     #############################
     background_music: bool = True
+    background_music_path: str = os.path.join(
+        base_dir,
+        'media_assets/fluffing_a_duck.mp3'
+    )
     target_resolution: tuple[int, int] = (1930, 1080)
+    outro_video_path: str = os.path.join(
+        base_dir,
+        'media_assets/like_share_and_subscribe.mp4'
+    )
 
     ##########################
     # Youtube Upload Related #
     ##########################
-    limit_hashtags: int = 50
+    your_youtube_username: Optional[str] = None
+    your_youtube_password: Optional[str] = None
+    client_secrets_file: str = os.path.join(base_dir, 'client_secrets.json')
+    limit_hashtags: int = 100
     imp_hashtags: list[str] = [
         "videoedit",
         "funney",
         "compilation",
+        "best memes compilation",
+        "memes",
+        "bruh sound",
+        "effect meme",
+        "fluffing_a_duck"
     ]
+    yt_video_title: str = "TRY NOT TO LAUGH 😆"
+    yt_video_sub_title: str
+    yt_video_description: str = (
+        "If you accepted this challange\n"
+        "Thanks for watching this video. I hope you liked it.\n"
+        "Don't forget to subscribe!\n\n"
+        "meme compilation v{}"  # Placeholder for meme compilation version
+        "\n\n"
+        "For business and queries contact {}"  # Placeholder for contact details # noqa: E501
+        "\n\n"
+        "𝘾𝙍𝙀𝘿𝙄𝙏𝙎\n"
+        "=====================================================\n\n"
+        "𝙁𝙤𝙡𝙡𝙤𝙬 𝙩𝙝𝙚𝙨𝙚 𝙖𝙬𝙚𝙨𝙤𝙢𝙚 𝙘𝙧𝙚𝙖𝙩𝙤𝙧𝙨\n"
+        "{}"  # Placeholder for creators
+        "\n\n"
+        "---------------------------------------------------------------------------------------------------------------\n"  # noqa: E501
+
+        "All clips are used for entertainment purposes only! If there are any problems with the videos or songs featured send me an email at: streetcanultimate@gmail.com and we'll resolve the issue!\n"  # noqa: E501
+        "-----------------------------------------------------------------------------------------------------------------\n\n"  # noqa: E501
+
+        "{}"  # Placeholder for any additional information
+    )
 
     class Config:
         env_prefix = "env_"
 
-
-settings = Settings()
+settings = Settings(_env_file='.env')
